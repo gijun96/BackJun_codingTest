4e1225990825cd3e8dc8b2e678a9a43266e4bc1c
남기준 <102777258+gijun96@users.noreply.github.com>
2024-10-09 16:19:38 +0900

    [Bronze V] Title: 사칙연산, Time: 4 ms, Memory: 860 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	input := strings.Split(scanner.Text(), " ")
+	a, _ := strconv.Atoi(input[0])
+	b, _ := strconv.Atoi(input[1])
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+}
