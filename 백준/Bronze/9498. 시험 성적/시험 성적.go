d929302deeae432c9b454a20eac7c088f8b72c8b
남기준 <102777258+gijun96@users.noreply.github.com>
2024-10-09 17:20:28 +0900

    [Bronze V] Title: 시험 성적, Time: 4 ms, Memory: 844 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	score, _ := strconv.Atoi(scanner.Text())
+
+	var grade string
+
+	if score >= 90 {
+		grade = "A"
+	} else if score >= 80 {
+		grade = "B"
+	} else if score >= 70 {
+		grade = "C"
+	} else if score >= 60 {
+		grade = "D"
+	} else {
+		grade = "F"
+	}
+
+	fmt.Println(grade)
+
+}
