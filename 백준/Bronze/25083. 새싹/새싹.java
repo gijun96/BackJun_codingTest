927837bdba94a29889ee74418084795a14eab5c4
남기준 <102777258+gijun96@users.noreply.github.com>
2025-01-01 21:46:36 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String result = "";
+      result += ("         ,r'\"7\n");
+        result += ("r`-_   ,'  ,/\n");
+        result += (" \\. \". L_r'\n");
+        result += ("   `~\\/\n");
+        result += ("      |\n");
+        result += ("      |\n");
+
+        System.out.println(result);
+
+    }
+}
