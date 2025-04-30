0c4a384dc2e96f108776d9b42258273e3ec183c6
남기준 <102777258+gijun96@users.noreply.github.com>
2025-04-30 12:42:12 +0900

    [Bronze III] Title: 직각삼각형, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,44 +1,51 @@
 import java.io.*;
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         try{
-     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Boolean end = false;
-        ArrayList<String> list = new ArrayList<>();
-        while(!end){
-            String[] line = br.readLine().split(" ");
 
-            int a= Integer.parseInt(line[0]);
-            int b= Integer.parseInt(line[1]);
-            int c= Integer.parseInt(line[2]);
-            if(a == 0 && b == 0&& c ==0){
-                end =true;
-                break;
+            while(true){
+
+                ArrayList<Integer> tris = new ArrayList<>();
+                String input = br.readLine();
+                int a = Integer.parseInt(input.split(" ")[0]);
+                int b = Integer.parseInt(input.split(" ")[1]);
+                int c = Integer.parseInt(input.split(" ")[2]);
+
+                if(a == 0 && b == 0 && c ==0){
+                    break;
+                }
+                tris.add(a);
+                tris.add(b);
+                tris.add(c);
+                // 오름차순 정렬
+                Collections.sort(tris);
+                int x = tris.get(0);
+                int y = tris.get(1);
+                int z = tris.get(2);
+
+                if(z*z == x*x + y*y){
+                    bw.write("right"+ "\n");
+                }else{
+                    bw.write("wrong" + "\n");
+                }
+
+                tris.clear();
             }
+            bw.flush();
+            bw.close();
 
-            if(((a*a) == (b*b)+(c*c))||
-                    ((b*b) == (a*a)+(c*c))||
-                    ((c*c) == (a*a)+(b*b))){
-                list.add("right");
-            }else{
-                list.add("wrong");
-            }
-
-
-        }
-        for(String r : list){
-            bw.write(r+"\n");
-        }
-        bw.flush();
-        bw.close();
 
         }catch (IOException e){
             throw new RuntimeException(e);
         }
 
     }
+
 }
